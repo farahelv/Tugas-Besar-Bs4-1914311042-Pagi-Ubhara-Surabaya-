58695fa79c26e59277adec31f6e51390bdff544f
farah elvariza <79438006+farahelv@users.noreply.github.com>
2021-05-26 19:04:09 +0700

    Add files via upload

---
+++ download.ps1
@@ -0,0 +1,21 @@
+﻿C:\Users\LENOVO\BS\file wav
+$list = Get-Content C:\Users\LENOVO\BS\link.txt
+
+$clnt = New-Object System.Net.WebClient
+
+foreach($url in $list) 
+{ 
+
+	#Get the filename 
+	$filename = [System.IO.Path]::GetFileName($url) 
+
+	#Create the output path 
+	$file = [System.IO.Path]::Combine($pwd.Path, $filename) 
+
+	Write-Host -NoNewline "Getting ""$url""... "
+
+	#Download the file using the WebClient 
+	$clnt.DownloadFile($url, $file) 
+
+	Write-Host "done." 
+}
